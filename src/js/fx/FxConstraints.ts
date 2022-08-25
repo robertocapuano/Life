6a1375d0dd2966b7f98eafb4bd20b8cd51d4e6e3
Roberto Capuano <roberto.capuano@icloud.com>
2022-08-25 21:57:50 +0200

    Update FxConstraints.ts

--- src/js/fx/FxConstraints.ts
+++ src/js/fx/FxConstraints.ts
@@ -14,7 +14,7 @@ export function FxLinkConstraint(
     min_length: number, 
     max_length: number=min_length,
 ): FxConstraint
- {
+{
     return {
         apply: ( p: Array<vec2> ) => {
 
@@ -35,14 +35,13 @@ export function FxLinkConstraint(
     };
 }
 
-
 export function FxAngle1PConstraint( 
     u: FxParticle, 
     pivot: vec2, 
     axis_ref: vec2, 
     angle: number,
 ): FxConstraint
- {
+{
     return {
         apply: ( p: Array<vec2> ) => {
 
@@ -62,16 +61,28 @@ export function FxAngle1PConstraint(
     };
 }
 
-// export function DistanceConstraint( u: number, pivot_pos: vec3, min_dist: number, max_dist: number=min_dist ): FxConstraint
-// {
-//     return ( p: Array<vec3> ) => {
+export function FxAngle2PConstraint( 
+    u: FxParticle, 
+    v: FxParticle, 
+    axis_ref: vec2, 
+    angle: number,
+): FxConstraint
+{
+    return {
+        apply: ( p: Array<vec2> ) => {
 
-//         const p_u = p[u];
-//         const diff = sub( p_u, pivot_pos );
-//         const dire = norm( diff );
-//         const dist = mag( diff );
-//         const new_dist = clamp( dist, min_dist, max_dist );
-        
-//         p_u.copyFrom( add( pivot_pos, dire.scaleInPlace( new_dist ) ) );
-//     };
-// }
+            const up_diff = p[u].sub( p[v] );
+            const up_dist = up_diff.mag();
+            const up_dir = up_diff.normalize();
+            
+            const rot_angle = up_dir.angle( axis_ref );
+
+            const new_angle = clamp( rot_angle, -angle, +angle );
+            const new_dir = axis_ref.add( VEC2( cos(new_angle), sin(new_angle) )) ;
+
+            const u_pos = p[u].add( new_dir.scale( up_dist ) );
+            p[u] = u_pos;
+        },
+        has: ( p: FxParticle ) => ( p === u ),
+    };
+}
